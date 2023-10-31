91285f89ce5f99067ff44a3a1f2100a5b911941a
서정권 <sjg3234@gmail.com>
2023-10-31 11:50:50 +0900

    babel #finish

---
+++ 1.basics/ex06/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+    // const App = document.createElement('div');
+    // App.textContent = "Hello World";
+    // const App = React.createElement('div', null, 'Hello World');
+    // return App;
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+export {App};
