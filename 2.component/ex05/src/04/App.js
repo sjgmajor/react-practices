b71e64aab46f13128659d1fe77521d24e6d2cadf
서정권 <sjg3234@gmail.com>
2023-11-03 14:51:42 +0900

    commit

---
+++ 2.component/ex05/src/04/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import './assets/scss/App.scss'
+import Clock from './Clock';
+
+export default function App() {
+    const [ticks, setTicks] = useState(0);
+
+    setInterval(() => {
+        setTicks(ticks+1);
+        console.log(ticks);
+    }, 1000);
+
+    return (
+        <>
+            <span>{ticks}</span>
+            <Clock
+                message={'ex05: useEffect Hook example'}
+                hours={15}
+                minutes={5}
+                seconds={2}
+            />
+        </>
+    );
+}
